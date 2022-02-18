415bea89fd6fb193988d5d4ba5fca06b86d66dd6
mohanapriya1503 <saipriya150396@gmail.com>
2022-02-18 18:37:43 +0530

    Task

---
+++ src/Priya/Split_Concept.java
@@ -0,0 +1,12 @@
+package Priya;
+
+public class Split_Concept {
+
+	public static void main(String[] args) {
+		 String s="Welcome to Java World";
+		 String[] a = s.split(" ");
+		 System.out.println();
+
+	}
+
+}
